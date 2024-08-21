3cbe3534f14617dc4e2ebffc7c6c5484c492a455
itzzme-adhi <adhityamathi25@gmail.com>
2024-08-21 22:23:49 +0530

    Add files via upload

---
+++ evenorodd.py
@@ -0,0 +1,10 @@
+x=int(input("enter a number"))
+if(x<0):
+    print("negative")
+elif(x==0):
+    print("0 is an even number")
+else:
+    if(x%2==0):
+        print("its an even num")
+    else:
+        print("its is an odd num")
